5a1f68a18e2829f7884a9b3208de5218d4fe5383
galih.hayuningtyas@outlook.com <galih.hayuningtyas@outlook.com>
2020-12-22 14:24:30 +0700

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  contentful: {
-    spaceId: 'kxyzxmzhx6jd',
-    accessToken: 'Xkx-vpSRcgww8hEmqhZ4U7hhsYJudCNUZt6rPUrjpzs',
-    preview_accessToken: 'KPlLipz1ZjfIpSEmLW2AinVfD4ICuFDsw7Hr-xkrT-0'
-
-  }
-};
